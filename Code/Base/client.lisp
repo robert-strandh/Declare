abd5ea470243e6d167fa61f66b89cb7ab53ca76b
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 10:46:30 +0100

    Add function CONS to the existing four defined by this library.

--- Code/Base/client.lisp
+++ Code/Base/client.lisp
@@ -2,15 +2,17 @@
 
 ;;;; This library takes expressions apart using only the four
 ;;;; functions FIRST, REST, NULL, and ATOM (and perhaps higher-level
-;;;; functions built upon these four).  These four functions are not
-;;;; the standard Common Lisp functions with those names, and they are
-;;;; instead defined by the library.  By doing it this way, we can
+;;;; functions built upon these four), and it constructs expressions
+;;;; using only the function CONS (and again, perhaps higher-level
+;;;; functions built upon this function).  These five functions are
+;;;; not the standard Common Lisp functions with those names, and they
+;;;; are instead defined by the library.  By doing it this way, we can
 ;;;; allow for representations of expressions other than
 ;;;; S-expressions, and in particular, we support Concrete Syntax
 ;;;; Trees (CSTs).  The way we do this is to have those functions
 ;;;; trampoline to functions defined in a CLIENT object.  Client code
 ;;;; can then decide on the concrete implementation of those functions
-;;;; without impacting the library code itself.
+;;;; without impacting the library code itself.  In addition
 
 ;;; This client class can be used as a superclass of a client-specific
 ;;; client class.  It uses the standard Common Lisp functions FIRST,
@@ -28,6 +30,9 @@
    (%atom-function
     :initform #'cl:atom
     :reader atom-function)
+   (%cons-function
+    :initform #'cl:cons
+    :reader cons-function)
    (%raw-function
     :initform #'cl:identity
     :reader raw-function)))
@@ -45,6 +50,9 @@
 
 (defun atom (expression)
   (funcall (atom-function *client*) expression))
+
+(defun cons (expression1 expression2)
+  (funcall (cons-function *client*) expression1 expression2))
 
 (defun raw (expression)
   (funcall (raw-function *client*) expression))
