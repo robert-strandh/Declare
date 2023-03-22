e0bb79a21dd47539f2487e201c6d06d5745bbe75
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 15:56:46 +0100

    Define a test function.

--- Code/Test/test.lisp
+++ Code/Test/test.lisp
@@ -5,3 +5,24 @@
 
 (defclass s-expression-client (d:s-expression-client)
   ())
+
+(defmethod d:ignored-declaration-identifiers
+    ((client s-expression-client))
+  *ignored-declaration-identifiers*)
+
+(defun test1 (client)
+  (assert (equal (d:canonicalize-declaration-specifiers
+                  client
+                  '((ignore a b c)
+                    (ignorable d e f)
+                    (dynamic-extent g h)
+                    (foo hello j k l)
+                    (ignorable)
+                    (bar 1 2 3)))
+                 '((ignore a) (ignore b) (ignore c)
+                   (ignorable d) (ignorable e) (ignorable f)
+                   (dynamic-extent g) (dynamic-extent h)))))
+
+(defun test ()
+  (let ((client (make-instance 's-expression-client)))
+    (test1 client)))
