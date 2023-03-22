edd9e0edf8e3554387060622d476f6da699d0f6a
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 11:13:00 +0100

    Shadow symbol NIL.

--- Code/Base/packages.lisp
+++ Code/Base/packages.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:declare
   (:use #:common-lisp)
-  (:shadow #:first #:rest #:null #:atom #:cons)
+  (:shadow #:first #:rest #:null #:atom #:cons #:nil)
   (:export
    #:canonicalize-declaration-specifier
    #:canonicalize-declaration))
