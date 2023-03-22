15ff966b9707bac618de54c0fa2a71ac21670215
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 11:13:11 +0100

    Add symbol macro for NIL and update comment accordingly.

--- Code/Base/client.lisp
+++ Code/Base/client.lisp
@@ -1,18 +1,22 @@
 (cl:in-package #:declare)
 
-;;;; This library takes expressions apart using only the four
-;;;; functions FIRST, REST, NULL, and ATOM (and perhaps higher-level
-;;;; functions built upon these four), and it constructs expressions
-;;;; using only the function CONS (and again, perhaps higher-level
-;;;; functions built upon this function).  These five functions are
-;;;; not the standard Common Lisp functions with those names, and they
-;;;; are instead defined by the library.  By doing it this way, we can
-;;;; allow for representations of expressions other than
-;;;; S-expressions, and in particular, we support Concrete Syntax
-;;;; Trees (CSTs).  The way we do this is to have those functions
-;;;; trampoline to functions defined in a CLIENT object.  Client code
-;;;; can then decide on the concrete implementation of those functions
-;;;; without impacting the library code itself.  In addition
+;;;; This library needs to construct and take apart expressions.  But
+;;;; we do not want to commit to a particular representation of those
+;;;; expressions.  In particular, we want to support both
+;;;; s-expressions and concrete syntax trees as possible
+;;;; representations.  For that reason, we want to abstract out the
+;;;; exact functions and objects used for this purpose.  To take
+;;;; expressions apart, we use only the four functions FIRST, REST,
+;;;; NULL, and ATOM (and perhaps higher-level functions built upon
+;;;; these four).  To construct expressions, we use only the function
+;;;; CONS and the object NIL.  All these names are specific to this
+;;;; library, and unrelated to the Common Lisp symbols with the same
+;;;; name.  We accomplish the desired behavior by having our functions
+;;;; trampoline to functions defined in the CLIENT object, and we
+;;;; define NIL as a symbol macro that accesses a client-specific
+;;;; object in the CLIENT object.  We also define a function RAW that,
+;;;; given some object in the chosen representation of expressions,
+;;;; returns the underlying Common Lisp object being represented.
 
 ;;; This client class can be used as a superclass of a client-specific
 ;;; client class.  It uses the standard Common Lisp functions FIRST,
@@ -33,6 +37,9 @@
    (%cons-function
     :initform #'cl:cons
     :reader cons-function)
+   (%nil-object
+    :initform cl:nil
+    :reader nil-object)
    (%raw-function
     :initform #'cl:identity
     :reader raw-function)))
@@ -53,6 +60,8 @@
 
 (defun cons (expression1 expression2)
   (funcall (cons-function *client*) expression1 expression2))
+
+(define-symbol-macro nil (nil-object *client*))
 
 (defun raw (expression)
   (funcall (raw-function *client*) expression))
