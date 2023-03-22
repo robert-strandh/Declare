bfc41718e8639244dcc138ea01cac337c3fb16f9
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 12:36:55 +0100

    Change the lambda list of CANONICALIZE-DECLARATION-SPECIFIER.

--- Code/Base/generic-functions.lisp
+++ Code/Base/generic-functions.lisp
@@ -14,15 +14,18 @@
 (defmethod ignored-declaration-identifiers (client)
   '())
 
-;;; This generic function takes a CLIENT object and the two parts of a
-;;; declaration specifier, typically the declaration identifier (a
-;;; symbol) and the arguments.  But because a declaration specifier
-;;; where the first element is a type specifier can be used as an
-;;; abbreviation for the TYPE declaration specifier, the
-;;; DECLARATION-IDENTIFIER argument can also be an arbitrary type
+;;; This generic function takes three arguments.  The first argument
+;;; is a CLIENT object.  The second argument is a declaration
+;;; identifier (as an ordinary Common Lisp symbol), or perhaps a type
+;;; specifier (see below).  The third argument is the entire
+;;; declaration specifier to canonicalize.  As hinted above, typically
+;;; the second argument is a declaration identifier.  But because a
+;;; declaration specifier where the first element is a type specifier
+;;; can be used as an abbreviation for the TYPE declaration specifier,
+;;; the DECLARATION-IDENTIFIER argument can also be an arbitrary type
 ;;; specifier.  We handle that case by defining a default method on
 ;;; this generic function.  That default method is called only when
 ;;; the DECLARATION-IDENTIFIER argument is known to be a type
 ;;; specifier.
 (defgeneric canonicalize-declaration-specifier
-    (client declaration-identifier arguments))
+    (client declaration-identifier declaration-specifier))
