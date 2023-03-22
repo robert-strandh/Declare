fc7c122f9076c035ea908e0a3403583dddf097e0
Robert Strandh <robert.strandh@gmail.com>
2023-03-22 10:47:11 +0100

    Define generic function CANONICALIZE-DECLARATION-SPECIFIER.

--- Code/Base/generic-functions.lisp
+++ Code/Base/generic-functions.lisp
@@ -13,3 +13,16 @@
 ;;; By default, we return the empty list.
 (defmethod ignored-declaration-identifiers (client)
   '())
+
+;;; This generic function takes a CLIENT object and the two parts of a
+;;; declaration specifier, typically the declaration identifier (a
+;;; symbol) and the arguments.  But because a declaration specifier
+;;; where the first element is a type specifier can be used as an
+;;; abbreviation for the TYPE declaration specifier, the
+;;; DECLARATION-IDENTIFIER argument can also be an arbitrary type
+;;; specifier.  We handle that case by defining a default method on
+;;; this generic function.  That default method is called only when
+;;; the DECLARATION-IDENTIFIER argument is known to be a type
+;;; specifier.
+(defgeneric canonicalize-declaration-specifier
+    (client declaration-identifier arguments))
